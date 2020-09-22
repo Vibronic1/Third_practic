fc1a5988bf38f4d3f802bd80c8ad19698f4c68bf
Vibronic <andrey.brester@yandex.ru>
2020-09-22 18:19:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace read_write_in_any_coding
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader sr = new StreamReader(@"boot.ini");
+            
+            StreamWriter sw = new StreamWriter(@"boot_utf7.txt", false, Encoding.UTF7); 
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
